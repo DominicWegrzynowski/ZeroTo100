cc86c973c5091ff11b456213bfdb34aa388b1955
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-08-09 14:03:07 -0400

    Started js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,35 @@
+//get the value from the interface 
+//controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-//Call hello world
-function helloWorld() {
-    alert("hello world!");
+    //validate input
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //call generate numbers
+    let numbers = generateNumbers(startValue, endValue);
+
+    //call displayNumbers
+}
+
+//generate numbers from the start value to the end value
+//logic function(s)
+function generateNumbers(startValue, endValue) {
+    
+    let numbers = [];
+    
+    for(let i = startValue; i <= endValue; i++) {
+        numbers.push(i);
+    }
+
+    return numbers;
+}
+
+//display numbers and mark the even ones bold
+//display or view function
+function displayNumbers() {
+    
 }
